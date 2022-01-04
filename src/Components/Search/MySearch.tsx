90132ac7d00f463dc4203e57baf0aacfbff02aaf
Baran Gezenoglu <kartalbarans3@hotmail.com>
2022-01-04 20:36:32 +0300

    Serch Filter logic added
    Search filter is working if input length >2

--- src/Components/Search/MySearch.tsx
+++ src/Components/Search/MySearch.tsx
@@ -2,9 +2,15 @@ import { Form, FormControl } from "react-bootstrap";
 import styles from "./MySearch.module.scss";
 export interface IMySearchProps {
   placeholder?: string;
+  onChangeHandler?: React.ChangeEventHandler<HTMLInputElement>;
+  inputValue?: string;
 }
 
-export const MySearch: React.FC<IMySearchProps> = ({ placeholder }) => {
+export const MySearch: React.FC<IMySearchProps> = ({
+  placeholder,
+  onChangeHandler,
+  inputValue,
+}) => {
   return (
     <Form className={styles.container}>
       <FormControl
@@ -12,6 +18,8 @@ export const MySearch: React.FC<IMySearchProps> = ({ placeholder }) => {
         placeholder={placeholder}
         className="me-2"
         aria-label="Search"
+        onChange={onChangeHandler}
+        value={inputValue}
       />
     </Form>
   );
